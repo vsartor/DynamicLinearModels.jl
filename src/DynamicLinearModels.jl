c2e35177515c9dcc2920fa40c70b0a78544a2f9b
Victhor Sartório <vct.sartor@gmail.com>
2019-08-30 17:00:15 -0300

    Add dimension check and simulation method

--- src/DynamicLinearModels.jl
+++ src/DynamicLinearModels.jl
@@ -1,5 +1,94 @@
 module DynamicLinearModels
 
-greet() = print("Hello World!")
+export dlm_dimension, simulate
+
+using Distributions
+using LinearAlgebra
+
+
+"""
+    dlm_dimension(F, G[, V, W])
+
+Computes the dimension of a Dynamic Linear Model based on the observational
+and evolutional matrices. The error covariance matrices and observations may
+be passed as well so that dimensions can be checked.
+"""
+function dlm_dimension(F::Matrix{Float64},
+                       G::Matrix{Float64},
+                       V::Union{Symmetric{Float64}, Nothing} = nothing,
+                       W::Union{Symmetric{Float64}, Nothing} = nothing)
+
+    n = size(F, 1)
+    p = size(F, 2)
+
+    if size(G, 1) != size(G, 2)
+        throw(DimensionMismatch("G must be a square matrix."))
+    end
+
+    if size(G) != (p, p)
+        throw(DimensionMismatch("Dimension of G dos not match that of F."))
+    end
+
+    if !isnothing(V)
+        # Only need to check one dimensions since Symmetric guarantees square
+        if size(V, 1) != n
+            throw(DimensionMismatch("Dimensions of V are a mismatch."))
+        end
+    end
+
+    if !isnothing(W)
+        # Only need to check one dimensions since Symmetric guarantees square
+        if size(W, 1) != p
+            throw(DimensionMismatch("Dimensions of W are a mismatch."))
+        end
+    end
+
+    return n, p
+end
+
+
+"""
+    simulate(F, G, V, W, θ₀, T[, nreps])
+
+Simulates a Dynamic Linear Model specified by the quadruple (F, G, V, W)
+with a starting state of θ₁ <- N(G θ₀, W), with T observations. A parameters
+nreps may be passed indicating the number of replicates to be generated.
+
+Note that the parametrizations being considered in this package is such that
+    y[t] = F * y[t-1] + ϵ
+and not the notation from West and Harrison (1996) where
+    y[t] = F' * y[t-1] + ϵ.
+"""
+function simulate(F::Matrix{Float64},
+                  G::Matrix{Float64},
+                  V::Symmetric{Float64},
+                  W::Symmetric{Float64},
+                  θ₀::Vector{Float64},
+                  T::Integer,
+                  nreps::Integer = 1)
+    # TODO: Allow V and W to be lists
+
+    # TODO: Create a new method:
+    #         Allow for a discount factor to be passed (it is computed online
+    #         with an ongoing filtering routine, starting with a smart prior on
+    #         θ₀).
+
+    n, p = dlm_dimension(F, G, V, W)
+
+    θ = Array{Float64}(undef, T, p)
+    y = Array{Float64}(undef, T, n * nreps)
+
+    ω = MultivariateNormal(zeros(p), W)
+    ϵ = MultivariateNormal(zeros(n), V)
+
+    θ[1,:] = G * θ₀ + rand(ω)
+    y[1,:] = F * θ[1,:] + rand(ϵ)
+    for t = 2:T
+        θ[t,:] = G * θ[t-1,:] + rand(ω)
+        y[t,:] = F * θ[t,:] + rand(ϵ)
+    end
+
+    return θ, y
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,41 @@
 using DynamicLinearModels
+using LinearAlgebra
 using Test
 
-@testset "DynamicLinearModels.jl" begin
-    # Write your own tests here.
+@testset "Dimension Checking" begin
+    F = reshape([1., 0.], 1, 2)
+    G = [1. 1.; 0. 1.]
+    V = reshape([1.], 1, 1)
+    W = [1. 0.; 0. 0.2]
+
+    @test_throws MethodError dlm_dimension(F, G, V, W)
+
+    F = reshape([1., 0.], 1, 2)
+    G = [1. 1.; 0. 1.]
+    V = Symmetric(reshape([1.], 1, 1))
+    W = Symmetric([1. 0.; 0. 0.2])
+
+    @test dlm_dimension(F, G, V, W) == (1, 2)
+    @test dlm_dimension(F, G) == (1, 2)
+
+    F = reshape([1., 0.], 1, 2)
+    G = [1. 1.; 0. 1.]
+    V = Symmetric(reshape([1.], 1, 1))
+    W = Symmetric([1. 0. 0.; 0. 2. 0.; 0. 0. 0.2])
+
+    @test_throws DimensionMismatch dlm_dimension(F, G, V, W)
+
+    F = reshape([1., 0.], 1, 2)
+    G = [1. 1.; 0. 1.]
+    V = Symmetric([1. 0.; 0. 1.])
+    W = Symmetric([1. 0.; 0. 2.])
+
+    @test_throws DimensionMismatch dlm_dimension(F, G, V, W)
+
+    F = reshape([1., 0.], 1, 2)
+    G = [1. 1. 1.; 0. 1. 1.; 0. 0. 0.]
+    V = Symmetric(reshape([1.], 1, 1))
+    W = Symmetric([1. 0.; 0. 2.])
+
+    @test_throws DimensionMismatch dlm_dimension(F, G, V, W)
 end
