a5f1f181d021676844c4effbaf40420ab0e57a08
Victhor Sartório <vct.sartor@gmail.com>
2019-08-31 16:08:16 -0300

    Discount factor support in filter and forecast

--- src/DynamicLinearModels.jl
+++ src/DynamicLinearModels.jl
@@ -39,7 +39,7 @@ matrices and observations may be passed as well so that dimensions can be
 checked.
 """
 function dlm_dimension(F::Matrix{RT},
-                       G::Matrix{RT},
+                       G::Matrix{RT};
                        V::Union{CovMat{RT}, Nothing} = nothing,
                        W::Union{CovMat{RT}, Nothing} = nothing,
                        Y::Union{Vector{Vector{RT}}, Nothing} = nothing) where RT <: Real
@@ -108,7 +108,7 @@ function simulate(F::Matrix{RT},
     #         with an ongoing filtering routine, starting with a smart prior on
     #         θ₀).
 
-    n, p = dlm_dimension(F, G, V, W)
+    n, p = dlm_dimension(F, G, V=V, W=W)
 
     θ = Vector{Vector{RT}}(undef, T)
     y = Vector{Vector{RT}}(undef, T)
@@ -156,7 +156,7 @@ end
 """
     filter(Y, F, G, V, W[, m₀, C₀])
 
-Filtering routine for the simples Dynamic Linear Model case where covariance
+Filtering routine for the simplest Dynamic Linear Model case where covariance
 matrices are known and constants. If the parameters for the prior multivariate
 normal distribution is not given, smart values that have little effect on the
 result are chosen.
@@ -170,10 +170,9 @@ function filter(Y::Vector{Vector{RT}},
                 C₀::Union{CovMat{RT}, Nothing} = nothing) where RT <: Real
 
     #TODO: Create new methods:
-    #        - One that uses discount factors.
     #        - One that uses discount factors and uses stochastic variance.
 
-    n, p = dlm_dimension(F, G, V, W, Y)
+    n, p = dlm_dimension(F, G, V=V, W=W, Y=Y)
     T = size(Y, 1)
 
     m₀, C₀ = dlm_set_prior(Y, F, m₀, C₀)
@@ -194,6 +193,54 @@ function filter(Y::Vector{Vector{RT}},
     for t = 2:T
         a[t] = G * m[t-1]
         R[t] = Symmetric(G * C[t-1] * G') + W
+        f = F * a[t]
+        Q = Symmetric(F * R[t] * F') + V
+        A = R[t] * F' * inv(Q)
+        m[t] = a[t] + A * (Y[t] - f)
+        C[t] = R[t] - Symmetric(A * Q * A')
+    end
+
+    return a, R, m, C
+end
+
+
+"""
+    filter(Y, F, G, V, δ[, m₀, C₀])
+
+Filtering routine for a discount factor Dynamic Linear Model where observational
+covariance is known and constants. If the parameters for the prior multivariate
+normal distribution is not given, smart values that have little effect on the
+result are chosen.
+"""
+function filter(Y::Vector{Vector{RT}},
+                F::Matrix{RT},
+                G::Matrix{RT},
+                V::CovMat{RT},
+                δ::RT,
+                m₀::Union{Vector{RT}, Nothing} = nothing,
+                C₀::Union{CovMat{RT}, Nothing} = nothing) where RT <: Real
+
+    n, p = dlm_dimension(F, G, V=V, Y=Y)
+    T = size(Y, 1)
+
+    m₀, C₀ = dlm_set_prior(Y, F, m₀, C₀)
+
+    a = Vector{Vector{RT}}(undef, T)
+    m = Vector{Vector{RT}}(undef, T)
+    R = Vector{CovMat{RT}}(undef, T)
+    C = Vector{CovMat{RT}}(undef, T)
+
+    a[1] = G * m₀
+    R[1] = Symmetric(G * C₀ * G') / δ
+    f = F * a[1]
+    Q = Symmetric(F * R[1] * F') + V
+    A = R[1] * F' * inv(Q)
+    m[1] = a[1] + A * (Y[1] - f)
+    C[1] = R[1] - Symmetric(A * Q * A')
+
+    for t = 2:T
+        a[t] = G * m[t-1]
+        R[t] = Symmetric(G * C[t-1] * G') / δ
         f = F * a[t]
         Q = Symmetric(F * R[t] * F') + V
         A = R[t] * F' * inv(Q)
@@ -248,8 +295,6 @@ function forecast(F::Matrix{RT},
                   m::Vector{Vector{RT}},
                   C::Vector{CovMat{RT}},
                   h::Integer) where RT <: Real
-    #TODO: Do not handle 1-step forecast, create another function for this for
-    #      efficiency
 
     f = similar(m, h)
     Q = similar(C, h)
@@ -267,6 +312,24 @@ function forecast(F::Matrix{RT},
     end
 
     return f, Q
+end
+
+
+"""
+    forecast(F, G, V, δ, s, S, h)
+
+Forecast routine for a discount factor Dynamic Linear Model with a horizon `h`.
+"""
+function forecast(F::Matrix{RT},
+                  G::Matrix{RT},
+                  V::CovMat{RT},
+                  δ::RT,
+                  m::Vector{Vector{RT}},
+                  C::Vector{CovMat{RT}},
+                  h::Integer) where RT <: Real
+    W = C[end] / δ
+
+    return forecast(F, G, V, W, m, C, h)
 end
 
 end # module
